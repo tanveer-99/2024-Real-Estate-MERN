340afe1b84b60e4e3bcd9a2398471e57ef950d40
tanveer-99 <tanvir3512@gmail.com>
2024-03-03 01:28:54 +0600

    complete sign up page functionaly

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 export const signup = async (req, res, next)=> {
     const {username, email, password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const hashedPassword = bcrypt.hashSync(password.toString(), 10);
     const newUser = new User({username, email, password: hashedPassword});
     try {
         await newUser.save();
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
     },
     {
         timestamps: true
-    }
+    },
 )
 
 const User = mongoose.model('User', userSchema);
