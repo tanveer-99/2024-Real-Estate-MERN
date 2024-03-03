80e95cc1d58c0917efe3fefa00a70384620d0078
tanveer-99 <tanvir3512@gmail.com>
2024-03-03 14:14:31 +0600

    complete sign in page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,10 +6,12 @@ const Header = () => {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                <span className='text-slate-500'>MDH</span>
-                <span className='text-slate-700'>State</span>
-            </h1>
+            <Link to='/'>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                    <span className='text-slate-500'>MDH</span>
+                    <span className='text-slate-700'>State</span>
+                </h1>
+            </Link>
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                 <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
                 <FaSearch className='text-slate-600'></FaSearch>
