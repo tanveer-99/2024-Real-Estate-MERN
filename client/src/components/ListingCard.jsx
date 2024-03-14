d3764c0fdf8706995ab2f0a8dc83244da0406e0d
tanveer-99 <tanvir3512@gmail.com>
2024-03-15 00:04:24 +0600

    create listing items component and complete listing search page

---
+++ client/src/components/ListingCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {MdLocationOn} from 'react-icons/md'
+
+const ListingCard = ({listing}) => {
+  return (
+    <div className=' bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
+        <Link to={`/listing/${listing._id}`}>
+            <img className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300' src={listing.imageURLs[0] || 'https://rb.gy/f4lmn7'} alt="listing_cover" />
+                <div className="p-3 flex flex-col gap-2 w-full">
+                <p className='text-xl font-semibold text-slate-700 truncate'>{listing.name}</p>
+                <div className="flex items-center gap-1">
+                    <MdLocationOn className="h-4 w-4 text-green-700" />
+                    <p className='truncate text-sm text-gray-600'>{listing.address}</p>
+                </div>
+                <div className="">
+                    <p className="text-sm text-gray-600 line-clamp-2">{listing.description}</p>
+                    <p className="font-semibold text-slate-500 mt-2">$ {listing.offer ? listing.discountPrice.toLocaleString('en-US') : listing.regularPrice.toLocaleString('en-US')} {listing.type === 'rent' && ' / month'}</p>
+                    
+                    <div className="text-slate-700 flex gap-4">
+                        <div className="font-bold text-xsm  ">
+                            {listing.bedrooms > 1 ? `${listing.bedrooms} beds ` : `${listing.bedrooms} bed `}
+                        </div>
+                        <div className="font-bold text-xsm ">
+                            {listing.bathrooms > 1 ? `${listing.bathrooms} baths ` : `${listing.bathrooms} bath `}
+                        </div>
+                        
+                    </div>
+                </div>
+                
+
+            </div>
+        </Link>
+        
+    </div>
+  )
+}
+
+export default ListingCard
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ListingCard from '../components/ListingCard';
 
 const Search = () => {
     const navigate = useNavigate();
@@ -130,8 +131,23 @@ const Search = () => {
                 <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Search</button>
             </form>
         </div>
-        <div className=''>
+        <div className='flex-1'>
             <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing Results:</h1>
+            <div className="p-7 flex flex-wrap gap-4">
+                {
+                    !loading && listing.length === 0 && (
+                        <p className='text-xl text-slate-700'>No Listing Found!</p>
+                    )
+                }
+                {
+                    loading && (
+                        <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                    )
+                }
+                {!loading && listing && listing.map(listing => 
+                    <ListingCard key={listing._id} listing={listing}></ListingCard>
+                ) }
+            </div>
         </div>
     </div>
   )
