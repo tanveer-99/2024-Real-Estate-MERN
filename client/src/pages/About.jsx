bba2c4c30f09222d777b57c422a63d07e740b1c9
tanveer-99 <tanvir3512@gmail.com>
2024-03-16 01:12:15 +0600

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About MDH Estate</h1>
+      <p className='mb-4 text-slate-700 '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas soluta maxime maiores ipsa eveniet a hic, praesentium sapiente facere cumque esse itaque quam obcaecati vero laudantium fugit neque iure mollitia.</p>
+      <p className='mb-4 text-slate-700 '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam beatae fuga earum eligendi, in incidunt dolorem reiciendis iste facere ipsum assumenda esse natus a voluptatum, enim quis ad minima ipsam.</p>
+      <p className='mb-4 text-slate-700 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate expedita placeat sed odit minus soluta modi? Sed cum nostrum aliquam neque, tempore architecto blanditiis accusamus, assumenda voluptates iste ipsam fugiat.</p>
+    </div>
   )
 }
 
